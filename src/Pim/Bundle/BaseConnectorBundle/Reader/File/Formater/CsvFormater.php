30c8676b068ca0704475564cc9e4f6a38b8733b3
Nicolas Dupont <nicolas@akeneo.com>
2015-03-11 11:14:03 +0100

    PIM-3832: add notes

--- src/Pim/Bundle/BaseConnectorBundle/Reader/File/Formater/CsvFormater.php
+++ src/Pim/Bundle/BaseConnectorBundle/Reader/File/Formater/CsvFormater.php
@@ -10,6 +10,8 @@ use Pim\Bundle\TransformBundle\Builder\FieldNameBuilder;
  * @author    Julien Sanchez <julien@akeneo.com>
  * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ * TODO: too "global" naming, content is product tinted
  */
 class CsvFormater
 {
